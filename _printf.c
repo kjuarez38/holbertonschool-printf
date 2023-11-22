a5bf6c0bc36c2957e4bb61ab8808b3d9013cbe09
kjuarez38 <kaisjuarez@gmail.com>
2023-11-22 01:14:33 -0800

    printf

--- _printf.c
+++ _printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 	va_list avar;
 	int totalLen = 0;
 
-	if (!format || (strlen(format) ==  && format[0] == '%'))
+	if (!format || (strlen(format) == 1 && format[0] == '%'))
 		/*Vérifier si la chaîne de format est NULL*/
 		return (-1);
 
