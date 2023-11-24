53081c6f1da9b441db97543b29490bde1e9ef00e
Jonathan Ismael <ismazpek@gmail.com>
2023-11-24 02:13:02 -0800

    is a fuction print char for printf project

--- _print_char.c
+++ _print_char.c
@@ -1,17 +1,43 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
- * _print_char - check the code
- * @avar: variadic argument
- * Return: 1 character
+ *_print_int - function
+ *@arg: argument variable
+ *Return: size
+ *
  */
-
-int _print_char(va_list avar)
+int _print_int(va_list arg)
 {
-	char c;
+	int n = va_arg(arg, int);
+	int r = 0;
+	int size = 0;
+	int i = 0;
+	int arr[20];
 
-	c = va_arg(avar, int);
-	putchar(c);
-	return (1);
+
+	if (n == 0)
+	{
+		_putchar('0' + 0);
+		size += 1;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		size += 1;
+	}
+	while (n != 0)
+	{
+		r = n % 10;
+		n = n / 10;
+		i++;
+		if (r < 0)
+			r = r * -1;
+		arr[i] = r;
+		size++;
+	}
+
+	while (i > 0)
+		_putchar('0' + arr[i--]);
+
+	return (size);
 }
